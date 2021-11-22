ac5d1f51ba986e9158c5098233febc24ad1681ac
Marathe <darshan.marathe@philips.com>
2021-11-22 19:20:09 +0530

    added rest of the functions in dynatable

--- src/stories/dynatable/dynaTable.jsx
+++ src/stories/dynatable/dynaTable.jsx
@@ -1,149 +1,216 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 //import TableFilter from 'tablefilter';
 
-
-import './dynaTable.css'
+import "./dynaTable.css";
 export default class DynaTable extends Component {
-    state = {
-        currentFilter: "",
-        prevFilter: "",
-        orderAsc: true,
-        sortedData: [],
-    }
+  state = {
+    currentFilter: "",
+    prevFilter: "",
+    orderAsc: true,
+    sortedData: [],
+  };
 
+  sortTable = (array, sortKey = true) => {
+    const sortBy = (p, orderAsc) => {
+      return array.slice(0).sort(function (a, b) {
+        if (orderAsc) return a[p] > b[p] ? 1 : a[p] < b[p] ? -1 : 0;
+        else return a[p] < b[p] ? 1 : a[p] > b[p] ? -1 : 0;
+      });
+    };
+    return sortBy(sortKey, this.state.orderAsc);
+  };
 
-    sortTable = (array, sortKey = true) => {
-        const sortBy = (p, orderAsc) => {
-            return array.slice(0).sort(function (a, b) {
-                if (orderAsc)
-                    return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
-                else
-                    return (a[p] < b[p]) ? 1 : (a[p] > b[p]) ? -1 : 0;
-            });
-        }
-        console.log(sortKey, this.state.orderAsc)
-        return sortBy(sortKey, this.state.orderAsc)
-    }
+  handleSort = (e) => {
+    const filter = e.target;
 
-    handleSort = (e) => {
-        const filter = e.target;
-        console.log(filter.dataset.filterValue)
-
-        if (!filter.dataset.filterValue) return false;
-        this.setState({ currentFilter: filter.dataset.filterValue }, () => {
-
-            this.toggleOrder();
-            this.init();
-        })
-
-    }
-    toggleOrder = () => {
-        if (this.state.currentFilter === this.state.prevFilter) {
-            this.setState({ orderAsc: !this.state.orderAsc })
-        } else {
-            this.setState({ orderAsc: true });
-        }
-    }
-
-
-    constructor(props) {
-        super(props)
-        this.state.sortedData = [...props.data.map((data, index) => {
-            return props.transFormFunc(data, index);
-        })];
-
-    }
-
-
-   async componentDidMount() {
+    if (!filter.dataset.filterValue) return false;
+    this.setState({ currentFilter: filter.dataset.filterValue }, () => {
+      this.toggleOrder(e);
       this.init();
-        // if (this.props.allowFilter) {
-        //     var tf = new TableFilter(this.props.id || 'table');
-        //     tf.init();
-        // }
+    });
+  };
+  toggleOrder = (e) => {
+    if (this.state.currentFilter === this.state.prevFilter) {
+      this.setState({ orderAsc: !this.state.orderAsc });
+      document.querySelectorAll(".up").forEach((e) => e.classList.remove("up"));
+      document
+        .querySelectorAll(".down")
+        .forEach((e) => e.classList.remove("down"));
+      e.target.classList.add("up");
+    } else {
+      this.setState({ orderAsc: true });
+      document.querySelectorAll(".up").forEach((e) => e.classList.remove("up"));
+      document
+        .querySelectorAll(".down")
+        .forEach((e) => e.classList.remove("down"));
+      e.target.classList.add("down");
     }
+  };
 
-    init() {
-        let newTableData = this.sortTable(this.props.data, this.state.currentFilter);
-        this.setState({
-            sortedData: newTableData,
-            prevFilter: this.state.currentFilter
-        });
+  constructor(props) {
+    super(props);
+    this.state.sortedData = [
+      ...props.data.map((data, index) => {
+        return props.transFormFunc(data, index);
+      }),
+    ];
+    if (this.props.editable && this.props.onTableEditFinish === null) {
+      throw new Error('need the "onTableEditFinish event"');
     }
+    this.state.selectable =
+      this.props.onRowSelected === null ? "" : "selectable";
+  }
 
-    static propTypes = {
-        data: PropTypes.array.isRequired,
-        className: PropTypes.string,
-        sortable: PropTypes.bool,
-        allowFilter: PropTypes.bool,
-        id: PropTypes.string,
-        editable: PropTypes.bool,
-        onTableEditFinish: PropTypes.func,
-        rowTemplate: PropTypes.func,
-        transFormFunc: PropTypes.func
+  async componentDidMount() {
+    this.init();
+    // if (this.props.allowFilter) {
+    //     var tf = new TableFilter(this.props.id || 'table');
+    //     tf.init();
+    // }
+  }
+
+  init() {
+    let newTableData = this.sortTable(
+      this.props.data,
+      this.state.currentFilter
+    );
+    this.setState({
+      sortedData: newTableData,
+      prevFilter: this.state.currentFilter,
+    });
+  }
+
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    className: PropTypes.string,
+    sortable: PropTypes.bool,
+    allowFilter: PropTypes.bool,
+    id: PropTypes.string,
+    editable: PropTypes.bool,
+    onTableEditFinish: PropTypes.func,
+    rowTemplate: PropTypes.func,
+    transFormFunc: PropTypes.func,
+    onRowSelected: PropTypes.func,
+  };
+
+  static defaultProps = {
+    sortable: false,
+    allowFilter: false,
+    id: "table",
+    className: "table",
+    editable: false,
+    transFormFunc: (data, index) => data,
+    onRowSelected: null,
+    onTableEditFinish: null,
+  };
+
+  GetTableHeader(prop) {
+    return (
+      <thead>
+        {this.props.sortable && (
+          <tr>
+            {Object.keys(prop).map((k, i) => (
+              <th
+                onClick={this.handleSort}
+                className={k === this.state.currentFilter ? "active" : ""}
+                data-filter-value={k}
+                key={k + "_" + i}
+              >
+                {" "}
+                {k}{" "}
+              </th>
+            ))}
+          </tr>
+        )}
+        {!this.props.sortable && (
+          <tr>
+            {Object.keys(prop).map((k, i) => (
+              <th key={k + "_" + i}> {k} </th>
+            ))}
+          </tr>
+        )}
+      </thead>
+    );
+  }
+
+  RowSelected = (e, rec, rind) => {
+    if (this.props.onRowSelected === null) return;
+    document
+      .querySelectorAll(".hilight")
+      .forEach((e) => e.classList.remove("hilight"));
+    e.target.parentNode.classList.add("hilight");
+    this.props.onRowSelected(rec, rind);
+  };
+  HandleEdit = (rowIndex, col, value) => {
+    const newArr = [...this.state.sortedData];
+    let row = newArr[rowIndex];
+    row[col] = value;
+    this.setState({ sortedData: newArr });
+    if (this.props.onTableEditFinish != null) {
+      this.props.onTableEditFinish(newArr);
     }
+  };
 
-    static defaultProps = {
-        sortable: false,
-        allowFilter: false,
-        id: 'table',
-        className: 'table',
-        editable: false,
-        transFormFunc: (data, index) => data
-    }
-
-    GetTableHeader(prop) {
-        return <thead>
-            {this.props.sortable && <tr>
-                {Object.keys(prop).map((k, i) => <th onClick={this.handleSort} className={(k === this.state.currentFilter) ? "active" : ""} data-filter-value={k} key={k + "_" + i}> {k} </th>)}
-                </tr>}
-            {!this.props.sortable && <tr>
-                {Object.keys(prop).map((k, i) => <th key={k + "_" + i}> {k} </th>)}
-            </tr>}
-        </thead>
-    }
-
-    HandleEdit = (rowIndex, col, value) => {
-        const newArr = [...this.state.sortedData]
-        let row = newArr[rowIndex];
-        row[col] = value;
-        this.setState({ sortedData: newArr });
-        if(this.props.onTableEditFinish != null){
-            this.props.onTableEditFinish(newArr)
-        }
-    }
-
-    GetRows(data = []) {
-        if (this.props.editable) {
-            return <tbody>
-                {this.state.sortedData.map((rec, rind) => {
-                    return <tr key={"row" + rind}>
-                        {Object.entries(rec).map(([K, v], i) => <td key={"val_" + rind + "_" + i}>
-                            <input type="text" onChange={(e) => {
-                                this.HandleEdit(rind, K, e.target.value)
-                            }} value={v} />
-                        </td>)} </tr>
-                })}
-            </tbody>
-        }
-
-        return <tbody>
-            {this.state.sortedData.map((rec, rind) => {
-                return this.props.rowTemplate == null ?
-                    <tr key={"row" + rind}>
-                        {Object.values(rec).map((v, i) => <td key={"val_" + rind + "_" + i}> {v} </td>)} </tr>
-                    : this.props.rowTemplate(rec, rind)
-            })}
+  GetRows(data = []) {
+    if (this.props.editable) {
+      return (
+        <tbody>
+          {this.state.sortedData.map((rec, rind) => {
+            return (
+              <tr
+                key={"row" + rind}
+                className={this.state.selectable}
+                onClick={(e) => this.RowSelected(e, rec, rind)}
+              >
+                {Object.entries(rec).map(([K, v], i) => (
+                  <td key={"val_" + rind + "_" + i}>
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        this.HandleEdit(rind, K, e.target.value);
+                      }}
+                      value={v}
+                    />
+                  </td>
+                ))}{" "}
+              </tr>
+            );
+          })}
         </tbody>
+      );
     }
 
-    render() {
-        return (
-            <table id={this.props.id || 'table'} className={this.props.className || 'table'}>
-                {this.GetTableHeader(this.props.data[0] || {})}
-                {this.GetRows(this.props.data)}
-            </table>
-        )
-    }
+    return (
+      <tbody>
+        {this.state.sortedData.map((rec, rind) => {
+          return this.props.rowTemplate == null ? (
+            <tr
+              key={"row" + rind}
+              className={this.state.selectable}
+              onClick={(e) => this.RowSelected(e, rec, rind)}
+            >
+              {Object.values(rec).map((v, i) => (
+                <td key={"val_" + rind + "_" + i}> {v} </td>
+              ))}{" "}
+            </tr>
+          ) : (
+            this.props.rowTemplate(rec, rind)
+          );
+        })}
+      </tbody>
+    );
+  }
+
+  render() {
+    return (
+      <table
+        id={this.props.id || "table"}
+        className={this.props.className || "table"}
+      >
+        {this.GetTableHeader(this.props.data[0] || {})}
+        {this.GetRows(this.props.data)}
+      </table>
+    );
+  }
 }
--- src/stories/dynatable/dynaTable.stories.jsx
+++ src/stories/dynatable/dynaTable.stories.jsx
@@ -1,55 +1,82 @@
-import React from 'react';
+import React from "react";
 import DynaTable from "./dynaTable";
 
-
-const data = [{
-    id : "A",
-    fname : "Tony",
-    lname : "Stark",
-    superName : "IronMan",
-    age : 30,
-    gender : "M",
-    powers : ["inteligence" , "money"]
-},
-{
-    id : "B",
-    fname : "Natasha",
-    lname : "Romonov",
-    superName : "Black widow",
-    age : 23,
-    gender : "F",
-    powers : ["inteligence" , "slow ageing"]
-},
-{
-    id : "C",
-    fname : "Clint",
-    lname : "Barton",
-    superName : "HawkEye",
-    age : 35,
-    gender : "M",
-    powers : ["inteligence" , "slow ageing" , "Archary"]
-},
-{
-    id : "E",
-    fname : "Thor",
-    lname : "Asgardian",
-    superName : "Thor",
-    age : 100,
-    gender : "M",
-    powers : ["Lighting" , "Super strength" , "Hammer"]
-}
-
-
-]
-
+const data = [
+  {
+    id: "A",
+    fname: "Tony",
+    lname: "Stark",
+    superName: "IronMan",
+    age: 30,
+    gender: "M",
+    powers: ["inteligence", "money"],
+  },
+  {
+    id: "B",
+    fname: "Natasha",
+    lname: "Romonov",
+    superName: "Black widow",
+    age: 23,
+    gender: "F",
+    powers: ["inteligence", "slow ageing"],
+  },
+  {
+    id: "C",
+    fname: "Clint",
+    lname: "Barton",
+    superName: "HawkEye",
+    age: 35,
+    gender: "M",
+    powers: ["inteligence", "slow ageing", "Archary"],
+  },
+  {
+    id: "E",
+    fname: "Thor",
+    lname: "Asgardian",
+    superName: "Thor",
+    age: 100,
+    gender: "M",
+    powers: ["Lighting", "Super strength", "Hammer"],
+  },
+];
 
 export default {
-    title : "Advanced/Dynamic Table",
-    component : DynaTable    
-}
+  title: "Advanced/Dynamic Table",
+  component: DynaTable,
+};
 
+export const Basic = () => <DynaTable id="simpleTable" data={data} />;
+export const Sortable = () => <DynaTable data={data} sortable={true} />;
+export const Selectable = () => (
+  <DynaTable
+    data={data}
+    sortable={true}
+    onRowSelected={(row, index) => {
+      console.log(row, index);
+    }}
+  />
+);
 
-export const Basic = () => <DynaTable id="simpleTable" data={data} />
-export const Sortable = () => <DynaTable data={data} sortable={true} />
+export const Ediable = () => (
+  <DynaTable
+    data={data}
+    editable={true}
+    onTableEditFinish={(arr) => {
+      console.log(arr);
+    }}
+  />
+);
+
+export const rowTemplate = () => (
+  <DynaTable
+    data={data}
+    rowTemplate={(rec, index) => (
+      <tr>
+        <td colspan="7"> {JSON.stringify(rec, "", 4)} </td>
+      </tr>
+    )}
+  />
+);
+
 // export const Filter = () => <DynaTable data={data} allowFilter={true} />
 // export const SortAndFilter = () => <DynaTable data={data} sortable={true} allowFilter={true} />
